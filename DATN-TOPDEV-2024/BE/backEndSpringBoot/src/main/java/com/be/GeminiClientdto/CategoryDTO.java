20d80080e5611f2e380a1995b2a465a7b170cc85
tienvv24 <voviettien2021@gmail.com>
2025-04-27 23:41:50 +0700

    commit Cần Ý fix lại thêm đánh Giá

--- DATN-TOPDEV-2024/BE/backEndSpringBoot/src/main/java/com/be/GeminiClientdto/CategoryDTO.java
+++
@@ -1,9 +0,0 @@
-package com.be.GeminiClientdto;
-
-import lombok.Data;
-
-@Data
-public class CategoryDTO {
-    private int categoryId;
-    private String name;
-}
--- DATN-TOPDEV-2024/BE/backEndSpringBoot/src/main/java/com/be/controller/UserController.java
+++ DATN-TOPDEV-2024/BE/backEndSpringBoot/src/main/java/com/be/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/users")  // Base URL for all user-related endpoints
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;  
@@ -75,7 +75,6 @@ public class UserController {
             String oldPassword = passwordRequest.get("oldPassword");
             String newPassword = passwordRequest.get("newPassword");
             String confirmPassword = passwordRequest.get("confirmPassword");
-
             // Gọi UserService để xử lý logic đổi mật khẩu
             userService.changePassword(userId, oldPassword, newPassword, confirmPassword);
 
